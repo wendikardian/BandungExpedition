0807b2730edd08069e060627e38d95a3df92349a
wendikardian <wendikardian@gmail.com>
2022-03-29 12:37:18 +0700

    hello

---
+++ android/app/src/main/kotlin/com/example/bandung_wisata/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bandung_wisata
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+
+import 'package:flutter/material.dart';
+import 'package:bandung_wisata/detail_screen.dart';
+import 'package:bandung_wisata/main_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      title : 'Wisata Bandung',
+      theme : ThemeData(),
+      home : MainScreen(),
+  );
+  } 
+}
+
